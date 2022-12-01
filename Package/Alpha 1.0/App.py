dec42f63a762b4b3db6f62a5f6f0ee134cfad91c
Cadity <34887947+Cadity@users.noreply.github.com>
2022-12-01 09:45:35 +0100

    Delete Package directory

--- Package/Alpha 1.0/App.py
+++
@@ -1,9 +0,0 @@
-from Window.Intro import DisplayIntro
-from Window.MainWindow import Display
-from Coronavirus.PingTest import check_ping
-
-result = DisplayIntro() #On affiche la fenêtre d'introduction
-
-if result == False: #Si la règle isClosed est fausse (donc que le fenêtre n'est pas fermé soit avec le clavier soit avec la "Croix")
-    Display() #On affiche la fenêtre principal
-
--- Package/Alpha 3.0/Readme.txt
+++
@@ -1,5 +0,0 @@
-Résumé des modifications :
-
-- Lecture du fichier GB et écriture dans info_seq_covid -> Etape B
-- Etape C -> Fichier fasta des séquences
-- Etape D -> Alignement
